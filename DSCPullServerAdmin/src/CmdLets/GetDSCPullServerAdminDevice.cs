02ed306220b2b8cc3c843b9deee5e1511079d307
Ben Gelens <ben@bgelens.nl>
2017-02-07 18:55:53 +0100

    added cmdlet Get-DSCPullServerAdminDevice

---
+++ DSCPullServerAdmin/src/CmdLets/GetDSCPullServerAdminDevice.cs
@@ -0,0 +1,59 @@
+﻿using DSCPullServerAdmin.src.Models;
+using Microsoft.Isam.Esent.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSCPullServerAdmin.src.CmdLets
+{
+    [Cmdlet(VerbsCommon.Get,
+        "DSCPullServerAdminDevice",
+        DefaultParameterSetName = "List")]
+    public class GetDSCPullServerAdminDevice : BaseCmdlet
+    {
+        public override string tableName
+        {
+            get
+            {
+                return "Devices";
+            }
+        }
+        protected override void ProcessRecord()
+        {
+            Api.MoveBeforeFirst(sesid, tableid);
+            while (Api.TryMoveNext(sesid, tableid))
+            {
+                IDictionary<string, JET_COLUMNID> columnDictionary = Api.GetColumnDictionary(sesid, tableid);
+                Device device = new Device();
+                device.TargetName = Api.RetrieveColumnAsString(sesid, tableid, columnDictionary["TargetName"]);
+                device.ConfigurationID = Api.RetrieveColumnAsString(sesid, tableid, columnDictionary["ConfigurationID"]);
+                device.ServerCheckSum = Api.RetrieveColumnAsString(sesid, tableid, columnDictionary["ServerCheckSum"]);
+                device.TargetChecksum = Api.RetrieveColumnAsString(sesid, tableid, columnDictionary["TargetChecksum"]);
+                if (Api.RetrieveColumnAsBoolean(sesid, tableid, columnDictionary["NodeCompliant"]).HasValue)
+                {
+                    device.NodeCompliant = Api.RetrieveColumnAsBoolean(sesid, tableid, columnDictionary["NodeCompliant"]).Value;
+                }
+                if (Api.RetrieveColumnAsDateTime(sesid, tableid, columnDictionary["LastComplianceTime"]).HasValue)
+                {
+                    device.LastComplianceTime = Api.RetrieveColumnAsDateTime(sesid, tableid, columnDictionary["LastComplianceTime"]).Value;
+                }
+                if (Api.RetrieveColumnAsDateTime(sesid, tableid, columnDictionary["LastHeartbeatTime"]).HasValue)
+                {
+                    device.LastHeartbeatTime = Api.RetrieveColumnAsDateTime(sesid, tableid, columnDictionary["LastHeartbeatTime"]).Value;
+                }
+                if (Api.RetrieveColumnAsBoolean(sesid, tableid, columnDictionary["Dirty"]).HasValue)
+                {
+                    device.Dirty = Api.RetrieveColumnAsBoolean(sesid, tableid, columnDictionary["Dirty"]).Value;
+                }
+                if (Api.RetrieveColumnAsInt32(sesid, tableid, columnDictionary["StatusCode"]).HasValue)
+                {
+                    device.StatusCode = Api.RetrieveColumnAsInt32(sesid, tableid, columnDictionary["StatusCode"]).Value;
+                }
+                WriteObject(device);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-#DSCPullServerAdmin
+# DSCPullServerAdmin
 An attempt to interface with DSC Pull Server devices.edb
 
 Primary goals:
-* Access reports more easily
-* Access v2 registered node information
-* Access v1 node information
-* Change node ConfigurationName server side
+- [x] Access reports more easily
+- [x] Access v2 registered node information
+- [x] Access v1 node information
+- [ ] Change node ConfigurationName server side\*<br/>
+\*edb file is locked when Pull Server is active. Probably won't work. For Get operations, maybe use VSS snapshotting if edb is locked
 
 Secondary goals:
-* Do the same with mdb :-)
+- [ ] Do the same with mdb :-)
+- [ ] Convert from edb to mdb
+- [ ] Convert from mdb to edb
 
 ![InitialCmdletOutput](images/initialcmdletoutput.png)

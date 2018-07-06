185e0365138754c97867c2cf84ef6c1fe9380c7e
Ben Gelens <ben@bgelens.nl>
2018-07-06 08:25:11 +0200

    added tests for connectionstring method

--- DSCPullServerAdmin/tests/Unit/classes/DSCPullServerSQLConnection.tests.ps1
+++ DSCPullServerAdmin/tests/Unit/classes/DSCPullServerSQLConnection.tests.ps1
@@ -18,5 +18,57 @@ InModuleScope $moduleName {
                 $instance.GetType().Name | Should -Be 'DSCPullServerSQLConnection'
             }
         }
+
+        Context 'ConnectionString' {
+            $testCases = @(
+                @{
+                    object = [DSCPullServerSQLConnection]::new(
+                        'bogusServer\instance',
+                        [pscredential]::new(
+                            'sa',
+                            (ConvertTo-SecureString 'bogusPass' -AsPlainText -Force)
+                        ),
+                        'bogusDb'
+                    )
+                    result = 'Server=bogusServer\instance;uid=sa;pwd=bogusPass;Trusted_Connection=False;Database=bogusDb;'
+                    caseDescription = 'server, credential, database'
+                },
+                @{
+                    object = [DSCPullServerSQLConnection]::new(
+                        'bogusServer\instance',
+                        [pscredential]::new(
+                            'sa',
+                            (ConvertTo-SecureString 'bogusPass' -AsPlainText -Force)
+                        )
+                    )
+                    result = 'Server=bogusServer\instance;uid=sa;pwd=bogusPass;Trusted_Connection=False;Database=DSC;'
+                    caseDescription = 'server, credential'
+                },
+                @{
+                    object = [DSCPullServerSQLConnection]::new(
+                        'bogusServer\instance',
+                        'bogusDb'
+                    )
+                    result = 'Server=bogusServer\instance;Integrated Security=True;Database=bogusDb;'
+                    caseDescription = 'server, database'
+                }
+                ,
+                @{
+                    object = [DSCPullServerSQLConnection]::new(
+                        'bogusServer\instance'
+                    )
+                    result = 'Server=bogusServer\instance;Integrated Security=True;'
+                    caseDescription = 'server'
+                }
+            )
+
+            It 'Should produce expected connectionstring based on constructor signature <caseDescription>' -TestCases $testCases {
+                param (
+                    $object,
+                    $result
+                )
+                $object.ConnectionString() | Should -Be $result
+            }
+        }
     }
 }

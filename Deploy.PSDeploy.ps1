01fbecf567ed899dab19f8219de9cd9687edd803
Ben Gelens <ben@bgelens.nl>
2018-10-27 10:41:38 +0200

    added github release test

--- Deploy.PSDeploy.ps1
+++ Deploy.PSDeploy.ps1
@@ -1,8 +1,7 @@
 if (
     $env:ProjectName -and $ENV:ProjectName.Count -eq 1 -and
     $env:BuildSystem -eq 'AppVeyor'
-)
-{
+) {
 
     if (!$Env:APPVEYOR_PULL_REQUEST_NUMBER -and 
         $Env:BuildSystem -eq 'AppVeyor' -and 
@@ -33,6 +32,43 @@ if (
                 }
             }
         }
+
+        #TODO: Replace with PSDeploy Script
+        Write-Host "Creating GitHub release" -ForegroundColor Green
+        $updatedManifest = Import-PowerShellDataFile .\BuildOutput\$Env:ProjectName\$Env:ProjectName.psd1
+
+        $releaseData = @{
+            tag_name = '{0}' -f $updatedManifest.ModuleVersion
+            target_commitish = $ENV:APPVEYOR_REPO_COMMIT
+            name = '{0}' -f $updatedManifest.ModuleVersion
+            body = $updatedManifest.PrivateData.PSData.ReleaseNotes
+            draft = $false
+            prerelease = $false
+        }
+
+        $releaseParams = @{
+            Uri = "https://api.github.com/repos/$ENV:APPVEYOR_REPO_NAME/releases?access_token=$Env:GitHubKey"
+            Method = 'POST'
+            ContentType = 'application/json'
+            Body = (ConvertTo-Json $releaseData -Compress)
+            UseBasicParsing = $true
+        }
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $newRelease = Invoke-RestMethod @releaseParams
+
+        Compress-Archive -DestinationPath ".\BuildOutput\DSCPullServerAdmin_$($updatedManifest.ModuleVersion).zip" -Path .\BuildOutput\$Env:ProjectName\*.*
+
+        $uploadParams = @{
+            Uri = ($newRelease.upload_url -replace '\{\?name.*\}', '?name=DSCPullServerAdmin_') +
+                $updatedManifest.ModuleVersion +
+                '.zip&access_token=' +
+                $Env:GitHubKey
+            Method = 'POST'
+            ContentType = 'application/zip'
+            InFile = ".\BuildOutput\DSCPullServerAdmin_$($updatedManifest.ModuleVersion).zip"
+        }
+
+        $null = Invoke-RestMethod @uploadParams
     }
 
     Deploy AppveyorDeployment {

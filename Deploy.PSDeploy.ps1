cc2d4ad399e5e6ad361433502972eda7e865aba8
Ben Gelens <ben@bgelens.nl>
2018-10-27 10:41:38 +0200

    added github release test

--- Deploy.PSDeploy.ps1
+++ Deploy.PSDeploy.ps1
@@ -1,8 +1,7 @@
 if (
     $env:ProjectName -and $ENV:ProjectName.Count -eq 1 -and
     $env:BuildSystem -eq 'AppVeyor'
-)
-{
+) {
 
     if (!$Env:APPVEYOR_PULL_REQUEST_NUMBER -and 
         $Env:BuildSystem -eq 'AppVeyor' -and 
@@ -49,6 +48,28 @@ if (
             Tagged Appveyor
         }
     }
+
+    #TODO: Replace with PSDeploy Script
+    $updatedManifest = Import-PowerShellDataFile .\BuildOutput\$Env:ProjectName\$Env:ProjectName.psd1
+
+    $releaseData = @{
+        tag_name = '{0}' -f $updatedManifest.ModuleVersion
+        target_commitish = $ENV:APPVEYOR_REPO_COMMIT
+        name = '{0}' -f $updatedManifest.ModuleVersion
+        body = $updatedManifest.PrivateData.PSData.ReleaseNotes
+        draft = $false
+        prerelease = $false
+    }
+
+    $releaseParams = @{
+        Uri = "https://api.github.com/repos/bgelens/dscpullserveradmin/releases?access_token=$Env:GitHubKey"
+        Method = 'POST'
+        ContentType = 'application/json'
+        Body = (ConvertTo-Json $releaseData -Compress)
+        UseBasicParsing = $true
+    }
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $null = Invoke-RestMethod @releaseParams
 } else {
     Write-Host "Not In AppVeyor. Skipped"
 }
